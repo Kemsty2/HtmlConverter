b9114bbdb9799a4aa5e89193da2de68cd2a36248
King Kemsty <kemsty2@yahoo.fr>
2020-11-04 23:35:59 +0100

    chore: Ajout d'une démo

---
+++ HtmlConverter.Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using HtmlConverter.Configurations;
+using System.IO;
+
+namespace HtmlConverter.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string html = "<div><strong>Hello</strong> World!</div>";
+            const string url = "www.google.com";
+
+            // Generate Pdf From HTML string
+
+            var result = Core.HtmlConverter.ConvertHtmlToPdf(html, new PdfConfiguration());
+
+            //  Store
+            File.WriteAllBytes("C:\\Users\\kemsty\\Pictures\\html.pdf", result);
+
+            // Generate Pdf From URL
+            result = Core.HtmlConverter.ConvertUrlToPdf(url, new PdfConfiguration());
+
+            //  Store
+            File.WriteAllBytes("C:\\Users\\kemsty\\Pictures\\google.pdf", result);
+
+            // Generate Pdf From HTML string
+            result = Core.HtmlConverter.ConvertHtmlToImage(html, new ImageConfiguration());
+
+            //  Store
+            File.WriteAllBytes("C:\\Users\\kemsty\\Pictures\\html.png", result);
+
+            // Generate Pdf From URL
+            result = Core.HtmlConverter.ConvertUrlToImage(url, new ImageConfiguration());
+
+            //  Store
+            File.WriteAllBytes("C:\\Users\\kemsty\\Pictures\\google.png", result);
+        }
+    }
+}
